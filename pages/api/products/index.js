9ae48ba3d25e4b2febbc95fb428ac9c22296be55
Mert Baykal <87305026+mert-baykal@users.noreply.github.com>
2023-09-28 07:30:50 +0000

    Update

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,7 +1,7 @@
 import { Dummy } from "..";
 
 export default function handler(req, res) {
-
+    // Created API
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(Dummy)
 }
